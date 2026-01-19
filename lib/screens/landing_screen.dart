0d7fcb3ae587b7d12ad67ee46ad84c2113b062d6
PraveenWE <praveentecsl@gmail.com>
2026-01-19 13:16:47 +0530

    change the uOR

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -84,7 +84,7 @@ class LandingScreen extends StatelessWidget {
                       ),
                     ),
                     child: const Text(
-                      'UNIVERSITY OF RUHUNA',
+                      'UNIVERSITY OF aRUHUNA',
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
