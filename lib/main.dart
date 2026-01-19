308e7821a54158cc5f2f1ce093b28b5e8f8d553a
PraveenWE <praveentecsl@gmail.com>
2026-01-19 11:32:56 +0530

    fix lot of errors in main branch

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
-import 'db/database_helper.dart';
 import 'screens/landing_screen.dart';
 import 'theme/app_theme.dart';
 
