abb0f6b024de2bf7245a0cdfcc24e1e94a6d4ba5
PraveenWE <praveentecsl@gmail.com>
2026-01-19 13:28:01 +0530

    reset all

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -239,7 +239,7 @@ class LandingScreen extends StatelessWidget {
                               Icon(Icons.login, size: 24),
                               SizedBox(width: 12),
                               Text(
-                                'Login sulitha 1122',
+                                'Login',
                                 style: TextStyle(
                                   fontSize: 18,
                                   fontWeight: FontWeight.bold,
